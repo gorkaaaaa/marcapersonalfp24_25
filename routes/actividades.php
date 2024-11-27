05b73e4a3863cd73204b42d55a80df9473a70eb2
alumno <659304@alu.murciaeduca.es>
2024-11-27 08:17:09 +0100

    Agrupacion de rutas por prefijo

--- routes/actividades.php
+++ routes/actividades.php
@@ -1,12 +1,15 @@
 <?php
 
 use App\Http\Controllers\ActividadController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Middleware\MyMiddleware;
 
+Route::group(['prefix' => 'actividades'], function(){
+    Route::get('/', [ActividadController::class, 'getIndex']);
 
-Route::get('actividades', [ActividadController::class, 'getIndex']);
+    Route::get('/show/{id}', [ActividadController::class, 'getShow'])->where('id', '[0-9]+');
 
-Route::get('actividades/show/{id}', [ActividadController::class, 'getShow'])->where('id', '[0-9]+');
+    Route::get('/create', [ActividadController::class, 'getCreate']);
 
-Route::get('actividades/create', [ActividadController::class, 'getCreate']);
-
-Route::get('actividades/edit/{id}', [ActividadController::class, 'getEdit'])->where('id', '[0-9]+');
+    Route::get('/edit/{id}', [ActividadController::class, 'getEdit'])->where('id', '[0-9]+');
+});
--- routes/curriculos.php
+++ routes/curriculos.php
@@ -4,13 +4,17 @@ use App\Http\Controllers\CurriculoController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\MyMiddleware;
 
-Route::get('curriculos', [CurriculoController::class, 'getIndex']);
+Route::group(['prefix' => 'curriculos'], function(){
+    Route::get('/', [CurriculoController::class, 'getIndex']);
 
-Route::get('curriculos/show/{id}', [CurriculoController::class, 'getShow'])
-->middleware(MyMiddleware::class)
-->where('id', '[0-9]+');
+    Route::get('/show/{id}', [CurriculoController::class, 'getShow'])
+    ->middleware(MyMiddleware::class)
+    ->where('id', '[0-9]+');
 
-Route::get('curriculos/create', [CurriculoController::class, 'getCreate']);
+    Route::get('/create', [CurriculoController::class, 'getCreate']);
 
-Route::get('curriculos/edit/{id}', [CurriculoController::class, 'getEdit'])
-->where('id', '[0-9]+');
+    Route::get('/edit/{id}', [CurriculoController::class, 'getEdit'])
+    ->where('id', '[0-9]+');
+});
+
+
