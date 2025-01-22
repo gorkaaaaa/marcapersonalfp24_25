08c406550f837787635763b9f046b542443683e4
Gorka <659304@alu.murciaeduca.es>
2025-01-22 09:49:31 +0100

    ApplyFilter y ApllySort en helper

---
+++ app/Helpers/FilterHelper.php
@@ -0,0 +1,27 @@
+<?php
+namespace App\Helpers;
+
+class FilterHelper
+{
+    public static function applyFilter($request, $filterColumns)
+    {
+        $modelClassName = $request->route()->controller->modelclass;
+        $query = $modelClassName::query();
+        $filterValue = $request->q;
+        if ($filterValue) {
+            $query->where(function ($query) use ($filterValue, $filterColumns) {
+                foreach ($filterColumns as $column) {
+                    $query->orWhere($column, 'like', '%' . $filterValue . '%');
+                }
+            });
+        }
+        return $query;
+    }
+
+    public static function applySort($request, $query)
+    {
+        $query->orderBy($request->_sort ?? 'id', $request->_order ?? 'asc');
+
+        return $query;
+    }
+}
